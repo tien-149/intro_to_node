59c392b5534ef5b5dc2772a28d4af24377b2bd6d
Le <t_le86524@ats-hr420-dm16.fc.ca>
2019-10-03 13:40:03 -0400

    week 5 crazy things

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    uname: "root",
+    upass: "root", //windows users leave this black
+    port: 8889, //3306
+    host: 'localhost',
+    db: 'bizcard'
+}
+
+module.exports = config;
